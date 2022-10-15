a9b523fcf42baf8c97d57c334a1a5169d8257d58
Bruno Seleghin <brunoseleghin@gmail.com>
2022-10-14 21:43:07 -0300

    Initial commit

---
+++ src/components/Heading.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading, HeadingProps } from './Heading';
+
+export default {
+  title: 'Compoenets/Heading',
+  component: Heading,
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
+  }
+} as Meta<HeadingProps>
+
+export const Default: StoryObj<HeadingProps> = {
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}
+
+export const Small: StoryObj<HeadingProps> = {
+  args: {
+    size: 'sm'
+  },
+}
+
+export const Large: StoryObj<HeadingProps> = {
+  args: {
+    size: 'lg'
+  },
+}
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <h1>Heading with H1 tag</h1>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+}
---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,54 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+  title: 'Compoenets/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    },
+    asChild: {
+      table: {
+        disable: true
+      }
+    },
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Text With P Tag</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+  }
+}
